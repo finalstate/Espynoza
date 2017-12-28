900aa01a4c5b271117043d08bbd79993cf94d9af
Rene Schmit <rene@black>
2017-12-28 08:51:57 +0100

    Rearranging/simplifying SimpleIO examples

--- etc/DemoDeviceList.py
+++ etc/DemoDeviceList.py
@@ -13,10 +13,10 @@ C_MQTTCredentials  =    {
 C_DeviceDescriptor =    { 
                          # Target      : (Config,      IP Address       Broker          Description                             ),
 
-                           'SimpleIO'  : ('SimpleIO',  '192.168.1.100', '192.168.1.99', 'Digital I/O and single-wire temp'      ), #
-                           'SimpleIO2' : ('SimpleIO2', '192.168.1.101', '192.168.1.99', 'Blink, temperature and rotary encoder' ), #
+                           'SimpleIO'  : ('SimpleIO',  '192.168.1.100', '192.168.1.99', 'Blink, temperature and rotary encoder' ),
+                           'SimpleIO2' : ('SimpleIO2', '192.168.1.101', '192.168.1.99', 'Digital I/O and single-wire temp'      ),
                                                        
-                           'Dimmer'    : ('Dimmer',    '192.168.1.102', '192.168.1.99', 'Dimmer with PWM'                       ), #
-                           'Neo'       : ('Neo',       '192.168.1.103', '192.168.1.99', 'NeoPixel'                              ), #
-                           'Baro'      : ('Baro',      '192.168.1.104', '192.168.1.99', 'Barometer, radar and temp'             ), #
+                           'Dimmer'    : ('Dimmer',    '192.168.1.102', '192.168.1.99', 'Dimmer with PWM'                       ),
+                           'Neo'       : ('Neo',       '192.168.1.103', '192.168.1.99', 'NeoPixel'                              ),
+                           'Baro'      : ('Baro',      '192.168.1.104', '192.168.1.99', 'Barometer, radar and temp'             ),
                         }
--- etc/SimpleIO2.py
+++ etc/SimpleIO2.py
@@ -4,8 +4,7 @@ C_Gateway        = '192.168.1.1'
 C_NetMask        = '255.255.255.0'
                  
 C_Hotspot        = 'NSA'
-                 
-C_ClientId       = 'SimpleIO2'
+C_ClientId       = 'SimpleIO'
 C_ChunkSize      = 256*8
 
 C_ConnectTO      = 15
@@ -25,24 +24,37 @@ C_LogFile        = ''
 #C_LogFile        = '/Log.txt'
              
 C_Handlers       = {
-                     'DS18B20'   : { 'Period' : 1000,  'Params' : (('Temperature', 0.067,    ), ) },
-                    
-                     'DigitalIn' : { 'Period' :  2,   'Params' : (('In1',  1),
-                                                                  ('In2',  1),
-                                                                 )
-                                   },
-                     'DigitalOut': { 'Period' : 100,   'Params' : (('Red',  True),
-                                                                  )
-                                   },
-                   }
+                     'AnalogIn'   : { 'Period' :   50,  'Params' : (('Analog',       20,      ), ) },
+                     'DigitalIn'  : { 'Period' :    1,  'Params' : (('Touch',       500,      ), ) },
+                     'Interrupt'  : { 'Period' :    1,  'Params' : (('Button1',               ),
+                                                                    ('Button2',               ),
+                                                                   ) 
+                                    },
+                     'DS18B20'    : { 'Period' : 1000,  'Params' : (('Temperature', 0.067,    ), ) },
+                                  
+                     'DigitalOut' : { 'Period' : 250,   'Params' : (('Red',  False),
+                                                                    ('Blue',  True)
+                                                                   )
+                                    },
+                     'PwmOut'     : { 'Period' : None,  'Params' : (('Green',                 ), ) },
+                    }
 
-# (Pin, direction: 0=IN | 1=OUT, Pull: 1=PULL_UP | None=None)
-C_Pins           = {
-                      'OneWire'   : ( 0, 1),  
-                    
-                      'In1'       : ( 4, 0, 1),  
-                      'In2'       : ( 5, 0, 1),  
-                      
+# (Pin, direction: 0=In, 1=Out)
+C_Pins            = {
+                      'OneWire'   : (0,  1),  
+                                  
+                      'Button1'   : (1,  0, None),
+                      'Button2'   : (3,  0, None),
+                                         
+                      'SDA'       : (4,  1),  
+                      'SCL'       : (5,  1),  
+                                         
+                      'Dimmer'    : (12, 1),  
+                                  
                       'Red'       : (14, 1),  
-                   }
+                      'Green'     : (12, 1),  
+                      'Blue'      : (13, 1),  
+                                    
+                      'Touch'     : (15, 0, None),  
+                    }
         
