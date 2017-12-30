efba2112d23de8dd76ff28625e1d3d9c1700c693
Rene Schmit <rene@black>
2017-12-30 10:17:03 +0100

    Added filter

--- EspyListen.py
+++ EspyListen.py
@@ -3,6 +3,7 @@
 ####################################################################################################
 
 import argparse
+import datetime
 import os
 import sys
 import time
@@ -51,7 +52,12 @@ def getResponse(p_Client, p_UserData, p_Message):
         
         l_Payload = l_Payload.decode()
         
-        print(f'''{l_Target:10s} : {l_Timestamp:>12s} {l_Tag:15s}({l_Id:10s}) -> {l_Payload}''')
+        if g_Arguments.datestamp:
+            l_Datestamp = f'{datetime.datetime.now():%Y-%m-%d %H:%M:%S}  '
+        else:
+            l_Datestamp = ''
+        
+        print(f'''{l_Datestamp}{l_Target:10s} : {l_Timestamp:>12s} {l_Tag:15s}({l_Id:5s}) -> {l_Payload}''')
         
     except Exception as l_Exception:
         print (l_Exception)
@@ -61,15 +67,17 @@ if __name__ == '__main__':
     l_ArgumentParser = argparse.ArgumentParser(description='Communicate with an ESP6288 board via MQTT')
     
     l_ArgumentParser.add_argument('-v', '--verbose',    required=False,  action='store_true',          help='Verbose user info')
+    l_ArgumentParser.add_argument('-d', '--datestamp',  required=False,  action='store_true',          help='Prepend datestamp')
+    
     l_ArgumentParser.add_argument('-b', '--broker',     required=True,   action='store',               help='Broker to access ESP.')
-    l_ArgumentParser.add_argument('-t', '--target',     required=False,  action='store', default=None, help='Only show these targets')
+    l_ArgumentParser.add_argument('-f', '--filter',     required=False,  action='store', default=None, help='Only show these targets')
     
     g_Arguments  = l_ArgumentParser.parse_args()    
         
     l_Broker = g_Arguments.broker
     l_UserName = DeviceList.C_MQTTCredentials[l_Broker][0]
     l_Password = DeviceList.C_MQTTCredentials[l_Broker][1]
-    l_Target   = '+' if g_Arguments.target == None else g_Arguments.target
+    l_Target   = '+' if g_Arguments.filter == None else g_Arguments.filter
     
     l_Client     = mqtt.Client()
         
